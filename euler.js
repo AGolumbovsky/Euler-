03978ece11e15476cdbb19335d45b13965b7a4ca
AGolumbovsky <agolumbovsky@gmail.com>
2015-06-17 18:17:36 -0600

    infinite loo

--- euler.js
+++ euler.js
@@ -51,5 +51,21 @@ var fb = function() {
 //The prime factors of 13195 are 5, 7, 13 and 29.
 //What is the largest prime factor of the number 600851475143 ?
 
+var num = 600851475143;
+var fac = num -1;
 
+var bigFac = function() {
+    // var x = true;
+    while (true) {
+        console.log("I'm alive...");
+        if (num % fac === 0) {
+            console.log(fac);
+            // x = false;
+            return fac;
+        } else {
+            fac--;
+        }
+    }
+}
 
+// ### this never finishes. maybe need algorithm ### 
